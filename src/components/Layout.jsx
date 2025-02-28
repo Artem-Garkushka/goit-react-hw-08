d06dc870f20686371dcd34d9a74fd291488f7217
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-02-28 21:39:09 +0200

    jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,15 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
 
-export const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
       <AppBar />
-      <Suspense fallback={null}>{children}</Suspense>
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
+
+export default Layout;

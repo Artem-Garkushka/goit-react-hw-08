7db194e42689ea1cbeac8aa68eb2b71fa7a795ec
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-02-28 22:39:49 +0200

    jsx

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
-import { fetchContacts, addContact, deleteContact } from "./Operations";
+import { fetchContacts, addContact, deleteContact } from "./contactsOperations";
 
 const contactsSlice = createSlice({
     name: "contacts",

524e95ee9af56a74ec52ff0b595a2b27ce38c4af
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-03-02 21:16:02 +0200

    jsx

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,17 +1,18 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = (state) => state.contacts.items;
 export const selectIsLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
+export const selectContacts = (state) => state.contacts.items;
+
+export const selectFilter = (state) => state.filters;
+
 export const selectFilteredContacts = createSelector(
-  [selectContacts, (state) => state.filters.name],
+  [selectContacts, selectFilter],
   (contacts, filter) => {
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(
-      (contact) =>
-        contact.name.toLowerCase().includes(normalizedFilter) ||
-        contact.number.includes(filter)
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   }
 );

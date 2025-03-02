aa21105ea107a4e96b61d6f7b00d2d26406c7f00
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-03-02 21:28:17 +0200

    jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,31 +1,37 @@
 import { useDispatch, useSelector } from "react-redux";
-import {
-  changeFilter,
-  selectNameFilter,
-  selectNumberFilter,
-} from "../../redux/filters/slice";
-import c from "./SearchBox.module.css";
-import { FaSistrix } from "react-icons/fa";
+import { changeNameFilter, changeNumberFilter } from '../../redux/filters/filtersSlice'; // Перевірте шлях до файлу
+import { selectNameFilter, selectNumberFilter } from '../../redux/filters/filtersSelectors'; // Перевірте шлях до файлу
+import css from './SearchBox.module.css';
 
 const SearchBox = () => {
   const dispatch = useDispatch();
-  const search = useSelector((selectNameFilter, selectNumberFilter));
+  const nameFilter = useSelector(selectNameFilter);
+  const numberFilter = useSelector(selectNumberFilter);
 
-  const onChange = (e) => dispatch(changeFilter(e.target.value));
+  const handleNameChange = (e) => {
+    dispatch(changeNameFilter(e.target.value));
+  };
+
+  const handleNumberChange = (e) => {
+    dispatch(changeNumberFilter(e.target.value));
+  };
 
   return (
-    <div className={c.searchBox}>
-      <label htmlFor="filter">Find contacts by name</label>
-      <div className={c.inputWrapper}>
-        <FaSistrix className={c.icon} />
-        <input
-          type="text"
-          id="filter"
-          value={search}
-          onChange={onChange}
-          placeholder="Name or number..."
-        />
-      </div>
+    <div className={css.searchBox}>
+      <input
+        type="text"
+        className={css.input}
+        value={nameFilter}
+        onChange={handleNameChange}
+        placeholder="Search by name..."
+      />
+      <input
+        type="text"
+        className={css.input}
+        value={numberFilter}
+        onChange={handleNumberChange}
+        placeholder="Search by number..."
+      />
     </div>
   );
 };

79a3e1bd494a29943cded08544a951db237864b7
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-03-04 21:48:02 +0200

    jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
-import {changeFilter} from "../../redux/contacts/slice";
+import {changeFilter} from "../../redux/filters/slice";
 import s from "./SearchBox.module.css";
-import {selectNameFilter} from "../../redux/filters/selectors";
+import {changeFilter} from "../../redux/filters/slice";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
